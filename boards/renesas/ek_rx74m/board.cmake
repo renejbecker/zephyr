3623730d6881b20703d91d4123d6d33800868ffb
Quy Tran <quy.tran.pz@renesas.com>
2025-08-04 10:08:18 +0000

    boards: renesas: rx: Add initial support for EK-RX74M board
    Add initial support for EK-RX74M board
    
    Signed-off-by: Quy Tran <quy.tran.pz@renesas.com>

---
+++ boards/renesas/ek_rx74m/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2025 Renesas Electronics Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(rfp "--device=RX74x")
+
+include(${ZEPHYR_BASE}/boards/common/rfp.board.cmake)
