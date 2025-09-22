f2dfe1fadcbf2b294a6c39d6be9115577d215bc9
Phi Tran <phi.tran.jg@bp.renesas.com>
2025-09-22 10:58:34 +0700

    clock: renesas: Enable ESWM power domain and fix binding
    - Update Renesas RX clock control driver to enable ESWM
    power domain during init
    - Correct DT binding property name from `stop_bit` to `stop-bit`
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>

--- drivers/clock_control/clock_control_renesas_rx_root_cgc.c
+++ drivers/clock_control/clock_control_renesas_rx_root_cgc.c
@@ -46,6 +46,24 @@ static int clock_control_rx_init(const struct device *dev)
 #elif CONFIG_HAS_RENESAS_RX_FSP
 	/* Initialize the system clock and peripheral clock */
 	bsp_clock_init();
+
+	FSP_HARDWARE_REGISTER_WAIT(R_SYSTEM->PDCTRESWM_b.PDCSF, 0);
+
+	if (1 == R_SYSTEM->PDCTRESWM_b.PDPGSF) {
+		/* Turn on ESWM power domain.
+		 * This requires MOCO to be enabled, but MOCO is always enabled after
+		 * bsp_clock_init().
+		 */
+		R_BSP_RegisterProtectDisable(BSP_REG_PROTECT_OM_LPC_BATT_SWR);
+		FSP_HARDWARE_REGISTER_WAIT((R_SYSTEM->PDCTRESWM & (R_SYSTEM_PDCTRESWM_PDCSF_Msk |
+								   R_SYSTEM_PDCTRESWM_PDPGSF_Msk)),
+					   R_SYSTEM_PDCTRESWM_PDPGSF_Msk);
+		R_SYSTEM->PDCTRESWM = 0;
+		FSP_HARDWARE_REGISTER_WAIT((R_SYSTEM->PDCTRESWM & (R_SYSTEM_PDCTRESWM_PDCSF_Msk |
+								   R_SYSTEM_PDCTRESWM_PDPGSF_Msk)),
+					   0);
+		R_BSP_RegisterProtectEnable(BSP_REG_PROTECT_OM_LPC_BATT_SWR);
+	}
 #endif
 	return 0;
 }
