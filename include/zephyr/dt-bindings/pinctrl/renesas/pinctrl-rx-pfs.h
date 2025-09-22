0e4ad1719be62877786bec7a5933b93af89b2b8c
Phi Tran <phi.tran.jg@bp.renesas.com>
2025-09-22 16:13:51 +0700

    drivers: pinctrl: Update pinctrl macro for support ethernet for RX74M
    Update POS, MARK and PSEL macro support ethernet for RX74M.
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>

--- include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-rx-pfs.h
+++ include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-rx-pfs.h
@@ -8,30 +8,34 @@
 #define ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_RENESAS_PINCTRL_RX_H__
 
 #define RX_PORT_NUM_POS  0
-#define RX_PORT_NUM_MASK 0xf
+#define RX_PORT_NUM_MASK 0x1f
 
-#define RX_PIN_NUM_POS  5
+#define RX_PIN_NUM_POS  6
 #define RX_PIN_NUM_MASK 0xf
 
-#define RX_PSEL_HIZ_JTAG_SWD 0x0
-#define RX_PSEL_SCI_0        0xA
-#define RX_PSEL_SCI_2        0xA
-#define RX_PSEL_SCI_4        0xA
-#define RX_PSEL_SCI_6        0xA
-#define RX_PSEL_SCI_8        0xA
-#define RX_PSEL_SCI_10       0xA
-#define RX_PSEL_SCI_12       0xA
-#define RX_PSEL_SCI_1        0xD
-#define RX_PSEL_SCI_3        0xD
-#define RX_PSEL_SCI_5        0xD
-#define RX_PSEL_SCI_7        0xD
-#define RX_PSEL_SCI_9        0xD
-#define RX_PSEL_SCI_11       0xD
+#define RX_PSEL_HIZ_JTAG_SWD      0x0
+#define RX_PSEL_SCI_0             0xA
+#define RX_PSEL_SCI_2             0xA
+#define RX_PSEL_SCI_4             0xA
+#define RX_PSEL_SCI_6             0xA
+#define RX_PSEL_SCI_8             0xA
+#define RX_PSEL_SCI_10            0xA
+#define RX_PSEL_SCI_12            0xA
+#define RX_PSEL_SCI_1             0xD
+#define RX_PSEL_SCI_3             0xD
+#define RX_PSEL_SCI_5             0xD
+#define RX_PSEL_SCI_7             0xD
+#define RX_PSEL_SCI_9             0xD
+#define RX_PSEL_SCI_11            0xD
+#define RX_PSEL_CLKOUT            0x1F
+#define RX_PSEL_ETH_RMAC_GMII_MII 0x20
+#define RX_PSEL_ETH_RMAC_RMII     0x21
+#define RX_PSEL_ETH_RMAC_RGMII    0x22
 
-#define RX_PSEL_POS  9
-#define RX_PSEL_MASK 0x1f
+#define RX_PSEL_POS  11
+#define RX_PSEL_MASK 0x7f
 
-#define RX_MODE_POS  16
+#define RX_MODE_POS  18
 #define RX_MODE_MASK 0x1
 
 #define RX_PSEL(psel, port_num, pin_num)                                                           \
