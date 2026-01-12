b5fe1a10e9d8b095691eab042ccf068990c90f5e
Quy Tran <quy.tran.pz@renesas.com>
2026-01-12 02:20:11 +0000

    soc: renesas: rx: Support sys_arch_reboot function for RX74M SoC
    The current sys_arch_reboot function in arch/rx is weak, add
    support for sys_arch_reboot in RX74M SOC using software reset
    
    Signed-off-by: Quy Tran <quy.tran.pz@renesas.com>

--- soc/renesas/rx/rx74m/soc.c
+++ soc/renesas/rx/rx74m/soc.c
@@ -33,3 +33,22 @@ void soc_early_init_hook(void)
 	g_protect_pfswe_counter = 0;
 	renesas_rx_register_protect_open();
 }
+
+static inline uint16_t BIT_U16(unsigned n)
+{
+	return (uint16_t)(1u << n);
+}
+
+void sys_arch_reboot(int type)
+{
+	ARG_UNUSED(type);
+
+	unsigned int key = irq_lock();
+
+	R_SYSTEM->PRCR = (uint16_t)((0xA5u << 8) | BIT_U16(1) | BIT_U16(5));
+
+	R_SYSTEM->SYRSTMSK0_b.SWMASK = 0u;
+	R_SYSTEM->SWRR_b.SWRR = 0xA501;
+
+	irq_unlock(key);
+}
