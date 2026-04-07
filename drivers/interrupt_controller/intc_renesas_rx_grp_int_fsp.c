76ccdd79032e92c1f81099cbaf712c490a3f1616
Duy Nguyen <duy.nguyen.xa@renesas.com>
2026-04-07 06:49:55 +0000

    drivers: interrupt: Change group interrupt handler flow for Renesas RX
    All of group interrupt signal can be processed same way, this change the
    switch case handling for the group interrupt handler annd remove the use
    of BSP_ICU_IRQ_HANDLER macro to ease up the maintainance for hal_renesas
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- drivers/interrupt_controller/intc_renesas_rx_grp_int_fsp.c
+++ drivers/interrupt_controller/intc_renesas_rx_grp_int_fsp.c
@@ -14,45 +14,16 @@
 #if DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) > 0
 
 /* Get the vector number from DTS.*/
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_il0))
 #define VECT_GROUP_IL0 DT_IRQN(DT_NODELABEL(group_irq_il0))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_bl0))
 #define VECT_GROUP_BL0 DT_IRQN(DT_NODELABEL(group_irq_bl0))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_bl1))
 #define VECT_GROUP_BL1 DT_IRQN(DT_NODELABEL(group_irq_bl1))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al0))
 #define VECT_GROUP_AL0 DT_IRQN(DT_NODELABEL(group_irq_al0))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al1))
 #define VECT_GROUP_AL1 DT_IRQN(DT_NODELABEL(group_irq_al1))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al2))
 #define VECT_GROUP_AL2 DT_IRQN(DT_NODELABEL(group_irq_al2))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al3))
 #define VECT_GROUP_AL3 DT_IRQN(DT_NODELABEL(group_irq_al3))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al4))
 #define VECT_GROUP_AL4 DT_IRQN(DT_NODELABEL(group_irq_al4))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al5))
 #define VECT_GROUP_AL5 DT_IRQN(DT_NODELABEL(group_irq_al5))
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al6))
 #define VECT_GROUP_AL6 DT_IRQN(DT_NODELABEL(group_irq_al6))
-#endif
 
 #define MIN_FACTOR_NUMBER 0
 #define MAX_FACTOR_NUMBER 31
@@ -72,63 +43,29 @@ struct rx_grp_int_cfg {
 bsp_grp_irq_cb_t g_bsp_group_irq_callback_list[BSP_FEATURE_GROUP_IRQ_NUM_INTERRUPTS]
 					      [BSP_FEATURE_GROUP_IRQ_NUM_FACTORS];
 extern void group_irq_isr(void);
+extern uint32_t bsp_group_irq_get_table_index(icu_event_t irq);
 
 /* Handlers for group interrupts */
 static void handler_group(const struct device *dev)
 {
 	DEV_CFG(cfg, dev);
+	uint32_t prev = g_current_isr_number;
 
 	switch (cfg->vector) {
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_il0))
 	case VECT_GROUP_IL0:
-		BSP_ICU_IRQ_HANDLER(17, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_bl0))
 	case VECT_GROUP_BL0:
-		BSP_ICU_IRQ_HANDLER(102, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_bl1))
 	case VECT_GROUP_BL1:
-		BSP_ICU_IRQ_HANDLER(103, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al0))
 	case VECT_GROUP_AL0:
-		BSP_ICU_IRQ_HANDLER(104, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al1))
 	case VECT_GROUP_AL1:
-		BSP_ICU_IRQ_HANDLER(105, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al2))
 	case VECT_GROUP_AL2:
-		BSP_ICU_IRQ_HANDLER(106, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al3))
 	case VECT_GROUP_AL3:
-		BSP_ICU_IRQ_HANDLER(107, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al4))
 	case VECT_GROUP_AL4:
-		BSP_ICU_IRQ_HANDLER(108, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al5))
 	case VECT_GROUP_AL5:
-		BSP_ICU_IRQ_HANDLER(109, group_irq_isr);
-		break;
-#endif
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(group_irq_al6))
 	case VECT_GROUP_AL6:
-		BSP_ICU_IRQ_HANDLER(110, group_irq_isr);
+		g_current_isr_number = cfg->vector;
+		group_irq_isr();
+		g_current_isr_number = prev;
 		break;
-#endif
 	default:
 		break;
 	}
@@ -200,7 +137,6 @@ int rx_grp_intc_callback_set(const struct device *dev, uint16_t factor, void (*c
 		return -EINVAL;
 	}
 
-	extern uint32_t bsp_group_irq_get_table_index(icu_event_t irq);
 	uint32_t index = bsp_group_irq_get_table_index((icu_event_t)cfg->vector);
 
 	g_bsp_group_irq_callback_list[index][factor] = callback;
